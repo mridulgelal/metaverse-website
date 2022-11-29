1b7899d6bf6532d355350afe538f13df5088f9fe
Mridul Gelal <52849122+mridulgelal@users.noreply.github.com>
2022-11-29 20:25:48 +0545

    Update GetStarted.jsx

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from "framer-motion";
-
 import { StartSteps, TitleText, TypingText } from "../components";
 import styles from "../styles";
 import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
