431e13bf1e4aae1c08a08014b5da294eceb72dcd
Mridul Gelal <52849122+mridulgelal@users.noreply.github.com>
2022-11-29 20:33:55 +0545

    Update About.jsx

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { motion } from "framer-motion";
-import { TypingText } from "../components/CustomTexts";
-import styles from "../styles";
-import { fadeIn, staggerContainer } from "../utils/motion";
+import { motion } from 'framer-motion';
+import { TypingText } from '../components/CustomTexts';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
   <section className={`${styles.paddings} z-10`}>
