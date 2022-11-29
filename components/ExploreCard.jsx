a3ab6d71a61c4c5b547fc370e0e80555f25b142d
Mridul Gelal <52849122+mridulgelal@users.noreply.github.com>
2022-11-29 20:31:47 +0545

    Update ExploreCard.jsx

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,8 +1,8 @@
 'use client'; 
 
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { fadeIn } from "../utils/motion";
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
