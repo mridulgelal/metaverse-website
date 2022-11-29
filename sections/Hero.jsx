4dcb40abb2d06f771bdcb80b50b6b365728c1917
Mridul Gelal <52849122+mridulgelal@users.noreply.github.com>
2022-11-29 20:35:34 +0545

    Update Hero.jsx

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
