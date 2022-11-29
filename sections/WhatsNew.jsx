94f0afc49738c807c2edd3471b6a575af95fb5fa
Mridul Gelal <52849122+mridulgelal@users.noreply.github.com>
2022-11-29 20:26:53 +0545

    Update WhatsNew.jsx

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,7 +1,6 @@
-"use client";
+'use client';
 
 import { motion } from "framer-motion";
-
 import { NewFeatures, TitleText, TypingText } from "../components";
 import styles from "../styles";
 import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
