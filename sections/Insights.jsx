7e413462cc0f94f0db48c2e0e79104f21c6348a7
Mridul Gelal <52849122+mridulgelal@users.noreply.github.com>
2022-11-29 20:36:04 +0545

    Update Insights.jsx

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { motion } from "framer-motion";
-import { InsightCard, TitleText, TypingText } from "../components";
-import styles from "../styles";
-import { staggerContainer } from "../utils/motion";
-import { insights } from "../constants";
+import { motion } from 'framer-motion';
+import { InsightCard, TitleText, TypingText } from '../components';
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { insights } from '../constants';
 
 const Insights = () => (
   <section className={`${styles.paddings}relative z-10`}>
