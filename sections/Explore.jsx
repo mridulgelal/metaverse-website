390599995404eea7ba0aaf09b7c5beebde31b6a9
Mridul Gelal <52849122+mridulgelal@users.noreply.github.com>
2022-11-29 20:25:03 +0545

    Update Explore.jsx

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,7 @@
-"use client";
+'use client';
 
 import { useState } from "react";
 import { motion } from "framer-motion";
-
 import styles from "../styles";
 import { exploreWorlds } from "../constants";
 import { staggerContainer } from "../utils/motion";
